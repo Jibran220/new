e3f70a7539a5e9a8ebe816051f1ae11091a484e4
Jibran220 <jibranjibran220@gmail.com>
2022-11-23 14:11:06 +0500

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+
+//router
+import { Switch,Route } from 'react-router'
+//layoutpages
+import Index from '../views/index'
+import Default from '../layouts/dashboard/default'
+import Horizontal from '../layouts/dashboard/horizontal'
+import Boxed from '../layouts/dashboard/boxed'
+import DualHorizontal from '../layouts/dashboard/dual-horizontal'
+import DualCompact from '../layouts/dashboard/dual-compact'
+import BoxedFancy from "../layouts/dashboard/boxed-fancy"
+import Simple from '../layouts/dashboard/simple'
+import Vendor from '../layouts/dashboard/Vendor'
+import Athvend from '../layouts/dashboard/Athvend'
+import Approver from '../layouts/dashboard/Approver'
+import ApproverAhead from '../layouts/dashboard/ApproverAhead'
+
+const IndexRouters = () => {
+    return (
+        <>
+            <Switch>
+            <Route exact path="/" component={Default}></Route>
+                <Route exact path="/ath" component={Vendor}></Route>
+                <Route exact path="/athpro/:id" component={Athvend}></Route>
+                <Route exact path="/approver" component={Approver}></Route>
+                <Route exact path="/ApproverAhead/:id" component={ApproverAhead}></Route>
+
+                <Route  path="/dashboard" component={Default}></Route>
+                <Route  path="/boxed" component={Boxed}></Route>
+                <Route  path="/horizontal" component={Horizontal}></Route>
+                <Route  path="/dual-horizontal" component={DualHorizontal}></Route>
+                <Route  path="/dual-compact" component={DualCompact} ></Route>
+                <Route  path="/boxedFancy" component={BoxedFancy} ></Route>
+                <Route  path="/auth" component={Simple}></Route>
+                <Route  path="/errors" component={Simple}></Route>
+            </Switch>
+        </>
+    )
+}
+
+export default IndexRouters
